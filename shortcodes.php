facd753246670acad995c773c6e5add9375a4549
Spencer Smith <dontaskspencer@gmail.com>
2016-12-31 16:39:00 -0700

    Added header_no_bottom_margin shortcode

--- shortcodes.php
+++ shortcodes.php
@@ -2,12 +2,19 @@
 #-=-=-=-=-=-=-
 # Shortcodes
 #-=-=-=-=-=-=-
+function short($sc_name){
+  add_shortcode($sc_name, $sc_name);
+}
 
 function no_title()
 {
   return "<style>.entry-title{display: none;}</style>";
 }
-add_shortcode('no_title', 'no_title');
-
+function header_no_bottom_margin()
+{
+  return "<style>#masthead{margin-bottom: 0;}</style>";
+}
+short('no_title');
+short('header_no_bottom_margin');
 
 ?>
